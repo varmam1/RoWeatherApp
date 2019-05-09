95d63a10ee36f877260973ab141485f6dad22f09
Manu Varma <varmam@bxscience.edu>
2019-05-09 21:00:26 +0100

    Added Unit change functionality and settings on/off functionality

--- MainController.java
+++ MainController.java
@@ -1,11 +1,15 @@
 package RoWeatherApp;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Map;
@@ -20,19 +24,37 @@ public class MainController {
     private ImageView flagPic;
 
     @FXML
+    private Button settings;
+
+    @FXML
+    private ToggleButton isFahrenheit;
+
+    @FXML
+    private AnchorPane settingsPage;
+
+    //Will convert from science units to freedom units (Celsius to Fahrenheit)
+    private int freedomUnitsConverter(double celsius){
+        return (int)Math.round((1.8*celsius) + 32);
+    }
+
+    @FXML
     private void initialize(){
         //This will get the temperature from the weather API and the feels like and display it in the info text
 
-        int feelsLike = (int) Math.round(CityDataFinder.getFeelsLikeTemperature(weather));
-        int actual = (int) Math.round(CityDataFinder.getTemperature(weather));
+        double feelsLike = CityDataFinder.getFeelsLikeTemperature(weather);
+        double actual = CityDataFinder.getTemperature(weather);
 
-        if (feelsLike == actual){
-            info.setText(feelsLike + "°");
-        }
-        else {
-            info.setText(feelsLike + "°" + "\n Actually: " + actual + "°");
+        int roundedFL = (int) Math.round(feelsLike);
+        int roundedActual = (int) Math.round(actual);
+
+        if (roundedActual == roundedFL) {
+            info.setText(roundedFL + "°");
+        } else {
+            info.setText(roundedFL + "°" + "\nActually: " + roundedActual + "°");
         }
 
+
+        //This should take the flag and display the correct one
         try {
             String f = FlagGetter.getFlagColor();
             if (f.equals("Green")){
@@ -53,6 +75,34 @@ public class MainController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        isFahrenheit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (!isFahrenheit.isSelected()){
+                    if (roundedActual == roundedFL) {
+                        info.setText(roundedFL + "°");
+                    } else {
+                        info.setText(roundedFL + "°" + "\nActually: " + roundedActual + "°");
+                    }
+                }
+                else {
+                    int fl = freedomUnitsConverter(CityDataFinder.getFeelsLikeTemperature(weather));
+                    int act = freedomUnitsConverter(CityDataFinder.getTemperature(weather));
+
+                    if (fl == act) {
+                        info.setText(fl + "°");
+                    } else {
+                        info.setText(fl + "°" + "\nActually: " + act + "°");
+                    }
+                }
+            }
+        });
+    }
+
+    @FXML
+    public void showSettings(ActionEvent event){
+        settingsPage.setVisible(!settingsPage.isVisible());
     }
 
 }
